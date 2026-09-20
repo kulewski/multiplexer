bf2085c6a8fb879bca885b646dc87e1cb7869fe8
Krzysztof Kulewski <kulewski@gmail.com>
2026-09-20 15:34:41 +0000

    Version 2.3.1

--- lib/release.cc
+++ lib/release.cc
@@ -3,8 +3,8 @@
 
 namespace mx {
 namespace release {
-const char* const version = "2.3.0";
-const char* const version_hash = "v2.3.0";
+const char* const version = "2.3.1";
+const char* const version_hash = "v2.3.1";
 const char* const version_short_hash = "2.3";
 }  // namespace release
 }  // namespace mx
--- multiplexer/release.py
+++ multiplexer/release.py
@@ -1,6 +1,6 @@
 """Version strings attached to every log entry. Placeholders; a release
 process may overwrite this file."""
 
-version = "2.3.0"
-version_hash = "v2.3.0"
+version = "2.3.1"
+version_hash = "v2.3.1"
 version_short_hash = "2.3"
